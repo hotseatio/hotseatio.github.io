95ec29144a04d6bcb994a9351bad9784a70a3f0c
Nathan Smith <nathan@nathansmith.io>
2021-05-27 10:49:30 -0700

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,84 @@
+const colors = require('tailwindcss/colors')
+
+const round = (num) =>
+  num
+    .toFixed(7)
+    .replace(/(\.[0-9]+?)0+$/, '$1')
+    .replace(/\.0$/, '')
+// const rem = (px) => `${round(px / 16)}rem`
+const em = (px, base) => `${round(px / base)}em`
+
 module.exports = {
-  purge: [],
   darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: colors.trueGray,
+      },
+      lineHeight: {
+        'extra-none': '0.75',
+      },
+      borderWidth: {
+        12: '12px',
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            hr: {
+              marginTop: em(36, 16),
+              marginBottom: em(24, 16),
+            },
+            a: {
+              'text-decoration': 'none',
+              color: 'var(--color-primary)',
+              'font-weight': 'bold',
+              'border-bottom': '1px solid var(--color-link-underline)',
+              'transition-duration': '50ms, 0.1s, 0.1s',
+              'transition-timing-function': 'linear',
+              'transition-property': 'border-color, color, padding-bottom',
+              '&:hover': {
+                opacity: '0.6',
+                'border-bottom': 'transparent',
+                'padding-bottom': '1px',
+              },
+            },
+          },
+        },
+        lg: {
+          css: {
+            img: {
+              marginTop: em(16, 16),
+              marginBottom: em(16, 16),
+            },
+          },
+        },
+        dark: {
+          css: {
+            color: theme('colors.gray.200'),
+            h1: {
+              color: theme('colors.gray.50'),
+            },
+            h2: {
+              color: theme('colors.gray.50'),
+            },
+            h3: {
+              color: theme('colors.gray.50'),
+            },
+            h4: {
+              color: theme('colors.gray.50'),
+            },
+            code: {
+              color: theme('colors.gray.50'),
+            },
+          },
+        },
+      }),
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      typography: ['dark'],
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 }
